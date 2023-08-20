dc2963b28f523e8d30ac47926fb5897307d381e1
stephin036 <stephin036@gmail.com>
2023-08-20 14:16:53 +0530

    changes-8

--- React/src/components/About.js
+++ React/src/components/About.js
@@ -1,10 +1,24 @@
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>This is About page</h2>
-        </div>
-    );
-};
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+    constructor() {
+        super();
+        console.log("Parent constructor called");
+    }
+    componentDidMount() {
+        console.log("Parent componentDidMount() called");
+    }
+    render() {
+        console.log("Parent render called");
+        return(
+            <div>
+                <h1>About</h1>
+                <h2>This is About page</h2>
+                <UserClass name={"Stephin"} location={"Kottayam"} />
+            </div>
+        );
+    };
+}
 
 export default About;
---
+++ React/src/components/UserClass.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class UserClass extends React.Component {
+    constructor() {
+        super();
+        console.log("Child constructor called");
+        this.state = {
+            userInfo: {
+            }
+        }
+    }
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/stephin36");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo: json
+         })
+    }
+
+    componentDidUpdate() {
+        console.log("Updated...");
+    };
+
+    componentWillUnmount() {
+        console.log("Unmounted..");
+    }
+    render() {
+        console.log("Child render called");
+        const {name, login, id } = this.state.userInfo;
+        return(
+            <div className="user-card">
+                <button onClick={ () => {
+                }}>Counter</button>
+                <h2>Name: {name}</h2>
+                <h2>Location: {login}</h2>
+                <h2>Contact: {id}</h2>
+            </div>
+        );
+    };
+}
+
+export default UserClass;
